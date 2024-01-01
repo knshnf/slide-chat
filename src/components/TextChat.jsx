d163e347405a7176ff43e56a801fc4887e26c8c3
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2024-01-02 06:30:16 +0800

    Get local media stream

--- src/components/TextChat.jsx
+++ src/components/TextChat.jsx
@@ -2,7 +2,7 @@ function TextChat() {
     return (
       <>
           <div className="text-chat-container">
-              <div className="text-log"> Connecting to the server... please wait. </div>
+              <div className="text-log"> Connecting to the server... </div>
               <div className="text-chat-actions"> 
                   <button className="next-btn"> Next </button>
                   <textarea name="" id=""></textarea>
--- src/components/VideoChat.jsx
+++ src/components/VideoChat.jsx
@@ -1,14 +1,33 @@
 import TextChat from './TextChat.jsx'
+import React, { useState } from "react";
 
 function VideoChat() {
+    let localStream;
+    
+    const setupDevice = () => {
+        console.log('setupDevice invoked');
+        navigator.getUserMedia({ audio: true, video: true }, (stream) => {
+            // render local stream on DOM
+            const localPlayer = document.getElementById('localPlayer');
+            localPlayer.srcObject = stream;
+            localStream = stream;
+        }, (error) => {
+            console.error('getUserMedia error:', error);
+        });
+    };
+
+    setupDevice()
+
     return (
       <>
         <div className="video-chat-container">
           <div className="media-container">
             <div className="media">
-              <video src=""></video>
+              <video playsInline autoPlay id='peerPlayer' className="videoPlayer"></video>
             </div>
-            <div className="media"></div>
+            <div className="media">
+              <video playsInline autoPlay id='localPlayer' className="videoPlayer"></video>
+            </div>
           </div>
           <TextChat/>
         </div>
