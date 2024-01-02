1db3625664b26927a4e8d20a226f07ea8f7d1de7
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2024-01-02 15:31:09 +0800

    video working

--- src/components/TextChat.jsx
+++ src/components/TextChat.jsx
@@ -1,15 +1,18 @@
+import React, { useState } from "react";
+
 function TextChat() {
     return (
       <>
           <div className="text-chat-container">
-              <div className="text-log"> Connecting to the server... </div>
+              <div className="text-log"> 
+                <p> Connecting to server... </p>
+              </div>
               <div className="text-chat-actions"> 
                   <button className="next-btn"> Next </button>
                   <textarea name="" id=""></textarea>
                   <button className="send-btn"> Send </button>
               </div>
           </div>
-        
       </>
     )
   }
