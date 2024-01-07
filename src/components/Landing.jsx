347561534b078e51d8b7b984b0b2b86208523d05
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2024-01-07 15:00:03 +0800

    Add field to let a user input interests

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,33 @@
+import React, { useState } from "react";
+import Interest from './Interest.jsx'
+
 function Landing({handleStartTextChat, handleStartVideoChat}) {
+  const [interests, setInterests] = useState([]);
+  const [interest, setInterest] = useState("");
+
+  const handleOnEnter = (e) => {
+    if(e.keyCode == 13 && e.shiftKey == false) {
+      e.preventDefault();
+      console.log('interest entered')
+
+      let trimmed = interest.trim();
+      if(trimmed == "") {
+        return;
+      }
+
+      let updatedInterests = [...interests, trimmed,];
+      setInterests(updatedInterests);
+      setInterest('');
+    }
+  }
+
+  const removeInterest = value => {
+    setInterests(oldInterests => {
+      return oldInterests.filter(interest => interest !== value)
+    })
+  }
+
+
 
   return (
     <>
@@ -7,6 +36,16 @@ function Landing({handleStartTextChat, handleStartVideoChat}) {
         <div className="chat-btns">
           <button type="button" onClick={() => handleStartTextChat()}> Text </button>
           <button onClick={() => handleStartVideoChat()}> Video </button>
+        </div>
+        <h2 className="chat-label"> What do you wanna talk about? </h2>
+        <div className="interests-container">
+          { interests.length > 0 && <div className="interests">
+            {interests.map((interest, index) =>
+              <Interest interest={interest}  key={index} removeInterest={removeInterest}/>
+            )}
+          </div>}
+          
+          <input className="interests-field" type="text" placeholder="Add your interests (optional)" value={interest} onKeyDown={handleOnEnter} onChange={e => setInterest(e.target.value)}/>
         </div>
       </div>
     </>
